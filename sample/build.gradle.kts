69defb1761a2a3738244a00e83c58d1e818ec8c2
bocheng.lao <xiaojinjincz@outlook.com>
2026-03-11 17:35:14 +0800

    init

---
+++ sample/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "io.github.xiaobaicz.compose.sample"
+    compileSdk {
+        version = release(36) { minorApiLevel = 1 }
+    }
+
+    defaultConfig {
+        applicationId = "io.github.xiaobaicz.compose.sample"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = Version.code
+        versionName = Version.name
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}
+
+object Version {
+    const val Y = 26
+    const val M = 3
+    const val D = 11
+    const val P = 0
+    val code get() = P + D * 100 + M * 10000 + Y * 1000000
+    val name get() = "$Y.$M.$D.$P"
+}
---
+++ sample/src/main/java/io/github/xiaobaicz/compose/sample/App.kt
@@ -0,0 +1,8 @@
+package io.github.xiaobaicz.compose.sample
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+
+}
